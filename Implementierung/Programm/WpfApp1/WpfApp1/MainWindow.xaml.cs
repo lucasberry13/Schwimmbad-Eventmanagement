e95dc4df8d23d13e7688dd46aaa67a88986f1a7b
lucasberry13 <lucasberresheim@gmail.com>
2025-02-27 13:25:49 +0100

    Teilnehmer Statistik hinzugefügt

--- Implementierung/Programm/WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ Implementierung/Programm/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -23,11 +23,61 @@ namespace WpfApp1
             InitializeComponent();
         }
 
-        
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            AktualisiereStatistik();
+        }
+
+        private void AktualisiereStatistik()
+        {
+            
+            using (var context = new EventContext())
+            {
+                
+                int gesamtTeilnehmer = context.Participants.Count();
+
+                
+                double durchschnittsalter = 0;
+                if (gesamtTeilnehmer > 0)
+                {
+                    durchschnittsalter = context.Participants.Average(p => p.Age);
+                }
+
+                
+                StatsPanel.Children.Clear();
+
+                
+                var titleBlock = new TextBlock
+                {
+                    Text = "Teilnehmer-Statistik",
+                    FontWeight = FontWeights.Bold,
+                    FontSize = 16,
+                    Margin = new Thickness(5),
+                    HorizontalAlignment = HorizontalAlignment.Center
+                };
+                StatsPanel.Children.Add(titleBlock);
+
+                
+                StatsPanel.Children.Add(new TextBlock
+                {
+                    Text = $"Gesamtanzahl der Teilnehmer: {gesamtTeilnehmer}",
+                    Margin = new Thickness(5)
+                });
+
+                
+                StatsPanel.Children.Add(new TextBlock
+                {
+                    Text = $"Durchschnittsalter: {durchschnittsalter:F2} Jahre",
+                    Margin = new Thickness(5)
+                });
+            }
+        }
+
         private void OpenEventAnz_Click(object sender, RoutedEventArgs e)
         {
             var eventAnz = new EventAnz();
             eventAnz.ShowDialog();
+            AktualisiereStatistik();
         }
 
         
@@ -35,17 +85,20 @@ namespace WpfApp1
         {
             var eventNeu = new EventNeu();
             eventNeu.ShowDialog();
+            AktualisiereStatistik();
         }
         private void OpenTeilnehmerVerwaltung_Click(object sender, RoutedEventArgs e)
         {
             var teilnehmerVerwaltung = new TeilnehmerVerwalten();
             teilnehmerVerwaltung.ShowDialog();
+            AktualisiereStatistik();
         }
 
         private void BtnAssignParticipant_Click(object sender, RoutedEventArgs e)
         {
             TeilnehmerZuEvent assignWindow = new TeilnehmerZuEvent();
-            assignWindow.ShowDialog(); 
+            assignWindow.ShowDialog();
+            AktualisiereStatistik();
         }
 
     }
